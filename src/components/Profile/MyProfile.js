8f2f7efd3986712689838ff4631b5ea15eea2e2b
Sam Mohan <samfranklinm@gmail.com>
2023-03-09 14:25:48 -0800

    changed project to linkup

--- src/components/Profile/MyProfile.js
+++ src/components/Profile/MyProfile.js
@@ -5,7 +5,19 @@ function MyProfile() {
         <div>
             <Header />
             <h1> My Profile </h1>
+            <Demo />
         </div>
     )
 };
+
+function Demo() {
+    return (
+        <div>
+            <h1> demo </h1>
+        </div>
+    )
+};
+
+
+
 export default MyProfile;
