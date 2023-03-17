a60f0a1e0eee377654f20a379fe1ff0a80a25947
Sam Mohan <samfranklinm@gmail.com>
2023-03-16 22:51:51 -0700

    fixed icon and user icon on header along with search and post bars

--- src/components/Header/HeaderOption.js
+++ src/components/Header/HeaderOption.js
@@ -7,7 +7,7 @@ export default function HeaderOption({ img, title, onClick }) {
 
     return (
         <div onClick={onClick} className="headerOption">
-            {img && <img style={{ width: '25px', height: '25px' }} src={img}></img>}
+            {img && <img style={{ width: '40px', height: '40px' }} src={img}></img>}
             <h3 className="headerOption__title">{title}</h3>
         </div>
     )
